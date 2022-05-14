063869315ebfef2b0063e41385ca846c0d8f9f46
Dants0 <gui.dg1@hotmail.com>
2022-05-14 00:10:46 -0300

    add features

--- backend/config/customExpress.js
+++ backend/config/customExpress.js
@@ -1,8 +1,13 @@
 const express = require('express')
 const consign = require('consign')
+const cors = require('cors')
 
 module.exports = () => {
     const app = express()
+
+    app.use(cors({
+        url: 'http://localhost:3000/alugar'
+    }))
 
     app.use(express.urlencoded({extended: true}))
     app.use(express.json())
