ac0ef3085f58b1a11b24a573f9d0491d869ea544
Lucas Bacelar De Souza <lucas.bacelar@grupoboticario.com.br>
2022-05-22 16:45:25 -0300

    feat(backend): add api error class

---
+++ backend/utils/APIError.js
@@ -0,0 +1,27 @@
+export class APIError {
+  constructor(code, message, reasons) {
+    this.code = code
+    this.message = message
+    this.reasons = [reasons] || []
+  }
+
+  static WrongParams(message, reasons) {
+    return new APIError(406, message, reasons)
+  }
+
+  static Unauthorized(message = 'Unauthorized', reasons) {
+    return new APIError(401, message, reasons)
+  }
+
+  static BadRequest(message, reasons) {
+    return new APIError(400, message, reasons)
+  }
+
+  static NotFound(message, reasons) {
+    return new APIError(404, message, reasons)
+  }
+
+  static Internal() {
+    return new APIError(500, 'Internal error')
+  }
+}
