7230f4c9055ab26204dfd5be13eb30cd89771077
Dants0 <gui.dg1@hotmail.com>
2022-05-31 12:34:21 -0300

    using axios for dates

--- frontend/src/components/RentDisplay/RentDisplay.jsx
+++ frontend/src/components/RentDisplay/RentDisplay.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import axios from "axios";
+import React, {useEffect, useState} from "react";
 
-const RentDisplay = () => {
+const baseURL = "http://localhost:3001/aluguel/carros";
+
+export default function RentDisplay() {
+  const [car, setCar] = useState()
+
+  useEffect(() => {
+    axios.get(baseURL)
+      .then((response) => setCar(response.data))
+      .catch((error) =>{
+        console.error('ocorreu um erro' + error)
+      })
+  },[])
+
+  // console.log(car.data.name)
+
   return (
-    <div>RentDisplay</div>
-  )
+    <>
+      <h1>{car.message}</h1>
+      {/* <p>{car.nome}</p> */}
+    </>
+  );
 }
-
-export default RentDisplay
