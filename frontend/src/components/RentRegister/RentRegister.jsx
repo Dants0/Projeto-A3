576baba0c057c5ebe0e5f6b419aabe549443f811
Dants0 <gui.dg1@hotmail.com>
2022-06-01 17:36:01 -0300

    add mini validation

--- frontend/src/components/RentRegister/RentRegister.jsx
+++ frontend/src/components/RentRegister/RentRegister.jsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import { useForm } from 'react-hook-form';
 import {ChatTeardrop, Check, FileSearch, IdentificationCard, User} from 'phosphor-react';
 import styles from './styles.module.scss';
 
+
+
 const RentRegister = () => {
+  const {register, handleSubmit} = useForm();
+  // const onSubmit = (d) => alert(JSON.stringify(d));
+  const onSubmit = (d) => console.log(d);
+  let counter = 0;
+
+
   return (
     <>
         <div className={styles.container}>
@@ -10,28 +19,29 @@ const RentRegister = () => {
             <h1>Formulário de Registro</h1>
           </div>
           <div className={styles.box}>
-            <form action="">
+            <form onSubmit={handleSubmit(onSubmit)}>
               <label htmlFor="">
-                <User size={20} color="white" /><input type="text" placeholder="Nome"/>
+                <User size={20} color="white" /><input type="text" placeholder="Nome" {...register("Nome", { required: true, maxLength: 20 })}/>
               </label>
               <label htmlFor="">
-                <User size={20} color="white"/><input type="text" placeholder="Sobrenome"/>
+                <User size={20} color="white"/><input type="text" placeholder="Sobrenome" {...register('Sobrenome')}/>
               </label>
               <label htmlFor="">
-                <IdentificationCard size={20} color="white"/><input type="text" placeholder="CPF"/>
+                <IdentificationCard size={20} color="white"/><input type="text" placeholder="CPF" {...register('CPF')} />
               </label>
               <label htmlFor="">
-                <IdentificationCard size={20} color="white"/><input type="text" placeholder="RG"/>
+                <IdentificationCard size={20} color="white"/><input type="text" placeholder="RG" {...register('RG')}/>
               </label>
               <label htmlFor="">
-                <Check size={20} color="white"/><input type="text" placeholder="Status"/>
+                <Check size={20} color="white"/><input type="text" placeholder="Status" {...register('Status')}/>
               </label>
               <label htmlFor="">
-                <ChatTeardrop size={20} color="white"/><textarea placeholder="Observações"></textarea>
+                <ChatTeardrop size={20} color="white"/><textarea placeholder="Observações" {...register('Observações')}></textarea>
               </label>
-              <label htmlFor="">
+              <label htmlFor="" id='importFile'>
                 <FileSearch size={20} color="white"/><input type="file"/>
               </label>
+              <p>Render: <span>{counter++}</span> </p>
               <input type="submit" id={styles.submitData} value='Cadastrar'/>
             </form>
           </div>
