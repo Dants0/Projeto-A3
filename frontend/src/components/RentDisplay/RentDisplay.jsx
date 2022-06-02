48dc2d5529cc1cb0db150ec56d50f0150a02cc89
Dants0 <gui.dg1@hotmail.com>
2022-06-01 21:59:10 -0300

    acessando data.data

--- frontend/src/components/RentDisplay/RentDisplay.jsx
+++ frontend/src/components/RentDisplay/RentDisplay.jsx
@@ -3,23 +3,27 @@ import styles from './styles.module.scss'
 import React, {useEffect, useState} from "react";
 
 
-const baseURL = "http://localhost:3001/aluguel/carros";
+const baseURL = "http://localhost:3001/aluguel/carros/54";
 
 const RentDisplay = () => {
     const [car, setCar] = useState(null)
 
     useEffect(() => {
       axios.get(baseURL).then((response) => {
-        setCar(response.data);
+        const data = response.data
+        setCar(data)
+        console.log(data);
       });
     }, []);
-
+    
     if(!car) return null;
+    if(!response) return null;
 
   return (
     <>
         <div className={styles.rentDisplayContainer}>
-          <h1>{car.message}</h1>
+          <h1>{response.message}</h1>
+          {/* <h1>{car.nome}</h1> */}
         </div>
     </>
   )
