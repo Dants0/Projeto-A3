c66659f453325d51ff2b0de80dcc5a92cb33167f
Dants0 <gui.dg1@hotmail.com>
2022-06-04 22:57:07 -0300

    feat(frontend): change in RentDisplay when i create one function for print id and name catch from api when click on button

--- frontend/src/components/RentDisplay/RentDisplay.jsx
+++ frontend/src/components/RentDisplay/RentDisplay.jsx
@@ -6,11 +6,17 @@ import { Pencil, Trash } from "phosphor-react";
 
 const baseURL = "http://localhost:3001/aluguel/carros";
 const imageURL = "http://localhost:3001/images/";
+const id = 10;
+const deleteURL = `http://localhost:3001/aluguel/carros/${id}`;
+
 
 const RentDisplay = () => {
+  
+  const [car, setCar] = useState([]) //pegando array de carros
+  
+  const [status, setStatus] = useState([]) //metodo delete card carros
 
-  const [car, setCar] = useState([])
-
+  //method get
   useEffect(() => {
     axios.get(baseURL)
       .then(response => {
@@ -19,23 +25,45 @@ const RentDisplay = () => {
       }).catch(err => {
         console.log(err)
       })
+
   }, [])
 
+
+  // method delete
   useEffect(() => {
-    axios.delete(baseURL)
-      // .then(()=>)
-  },[])
+    axios.delete(deleteURL)
+      .then(response => {
+        const deleteData = response.data.data
+        setStatus(deleteData)
+      }).catch(err => {
+        console.log(err)
+      })
+  }, [])
+
+  function imprimirDadosApi(){
+    car.map(i=>{
+      const deleteCar = () => {
+        let temp = `Id: ${i.id} e Nome: ${i.nome}`;
+        return temp;
+      };
+      return (
+        <>
+          {console.log(deleteCar())}
+        </>
+      )
+    })
+  }
 
   return (
     <>
       <div className={styles.container}>
         <div className={styles.card}>
-          {car.map(item => {
+          {car.map((item) => {
             return (
               <>
                 <div className={styles.cards}>
                   <div className={styles.controllButtons}>
-                    <span><button><Trash size={20} />Deletar</button></span>
+                    <span><button onClick={imprimirDadosApi}><Trash size={20} />Deletar</button></span>
                     <span><button><Pencil size={20} />Alterar</button></span>
                   </div>
                   <img src={imageURL + item.imagem} alt="Foto Da Api" />
