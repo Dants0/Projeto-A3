9a0dcfa97a952daef909613777e66cba2cf12a71
Dants0 <gui.dg1@hotmail.com>
2022-06-09 02:04:12 -0300

    add homecomponent

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Dashboard } from '../../components/Dashboard/Dashboard'
+import HomeDisplay from '../../components/HomeDisplay/HomeDisplay'
 
 const Home = () => {
   return (
     <>
         <div className="dashboard-container">
           <Dashboard/>
+          <HomeDisplay/>
         </div>
     </>
   )
